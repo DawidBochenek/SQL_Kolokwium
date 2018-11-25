f6b6baae61f0f1ab7954777e16d20bb607b55853
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-25 13:51:29 +0100

    modyf

--- SQL_Kolokwium/dbo/Tables/Student.sql
+++ SQL_Kolokwium/dbo/Tables/Student.sql
@@ -2,14 +2,16 @@
     [StudentId]  INT           NOT NULL,
     [Imie]       NVARCHAR (12) NULL,
     [Nazwisko]   NVARCHAR (40) NULL,
-    [Indeks]     INT           NOT NULL,
-    [Pesel]      INT           NOT NULL,
+    [Indeks]     INT           NULL,
+    [Pesel]      INT           NULL,
     [NazwaGrupy] NVARCHAR (8)  NULL,
-    [Rocznik]    NVARCHAR (5)  NULL,
+    [Rocznik]    NVARCHAR (9)  NULL,
     PRIMARY KEY CLUSTERED ([StudentId] ASC),
     UNIQUE NONCLUSTERED ([Indeks] ASC),
     UNIQUE NONCLUSTERED ([Pesel] ASC)
 );
+
+
 
 
 
