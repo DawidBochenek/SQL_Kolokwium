f48f53e85c4739c11d44be08ec930adde5a22ac7
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-25 14:08:38 +0100

    view 1

---
+++ SQL_Kolokwium/dbo/Views/StudentNieprzypisany.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW StudentNieprzypisany
+AS
+SELECT Student.Imie, Student.Nazwisko, Student.Indeks FROM Student LEFT JOIN Przypisanie ON Student.StudentId=Przypisanie.StudentId
+WHERE PrzedmiotId is null
