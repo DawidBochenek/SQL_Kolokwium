4237bf361ff6bafb4d34ef6c6777a43f6f817791
DawidBochenek <dawid.bochenek@hotmail.com>
2018-11-25 13:10:59 +0100

    Tabela Student

---
+++ SQL_Kolokwium/dbo/Tables/Student.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentId]  INT           NULL,
+    [Imie]       NVARCHAR (12) NULL,
+    [Nazwisko]   NVARCHAR (40) NULL,
+    [Indeks]     INT           NOT NULL,
+    [Pesel]      INT           NOT NULL,
+    [NazwaGrupy] NVARCHAR (8)  NULL,
+    [Rocznik]    NVARCHAR (5)  NULL,
+    UNIQUE NONCLUSTERED ([Indeks] ASC),
+    UNIQUE NONCLUSTERED ([Pesel] ASC)
+);
+
